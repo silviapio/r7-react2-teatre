9da3a6d64fdf54b4b8516096b0b0242be981360c
silviapio <silvia.piovesan@gmail.com>
2021-01-14 14:57:43 +0100

    exercise 1 solved (no styling)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import lines from "./linesData.json";
+import Line from "./Line";
 
 function App() {
+  const linesText = lines.map(item => <Line text={item} />);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {linesText}
     </div>
   );
 }
---
+++ src/Line.js
@@ -0,0 +1,7 @@
+function Line(props){
+    return(
+        <p>{props.text}</p>
+    )
+};
+
+export default Line;
